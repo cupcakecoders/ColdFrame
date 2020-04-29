4455e9fa3cf0fa56e2b86ac85e9fcd27dc429d4f
Kanwaljit <cavey.waveyy@gmail.com>
2020-04-30 00:10:08 +0100

    plants drop down

--- ColdFrame/ClientApp/src/components/Home.js
+++ ColdFrame/ClientApp/src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PlantsDropDown from './plants-dropdown/plants-dropdown'
 
 export class Home extends Component {
   static displayName = Home.name;
@@ -8,6 +9,7 @@ export class Home extends Component {
       <div>
         <h1>Hello, world!</h1>
         <p>Welcome to your new single-page application, built with:</p>
+          <PlantsDropDown/>
         <ul>
           <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
           <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
--- ColdFrame/ClientApp/src/components/plants-dropdown/plants-dropdown.js
+++ ColdFrame/ClientApp/src/components/plants-dropdown/plants-dropdown.js
@@ -1,12 +1,42 @@
-﻿import React from 'react';
-
-export const PlantNames = {
-    ReturnUrl: 'returnUrl',
-    Message: 'message'
-};
+﻿import React, { Component } from 'react';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import authService from "../api-authorization/AuthorizeService";
 
 export default class PlantsDropDown extends React.Component {
-    render () {
-        return //dropdown;
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: true
+        }
+        this.toggleDropdown = this.toggleDropdown.bind(this);
     }
+    
+    
+    componentDidMount() {
+    }
+    
+    toggleDropdown() {
+        this.setState({isOpen: !this.state.isOpen})
+    }
+    
+    render () {
+        return (
+            <Dropdown isOpen={this.state.isOpen} toggle={this.toggleDropdown}>
+                <DropdownToggle caret>
+                    Dropdown
+                </DropdownToggle>
+                <DropdownMenu>
+                    <DropdownItem header>Header</DropdownItem>
+                    <DropdownItem>Some Action</DropdownItem>
+                    <DropdownItem disabled>Action (disabled)</DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem>Foo Action</DropdownItem>
+                    <DropdownItem>Bar Action</DropdownItem>
+                    <DropdownItem>Quo Action</DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
+        );
+    }
+    
 }
